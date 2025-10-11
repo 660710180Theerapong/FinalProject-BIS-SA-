9d6d65ceb6da0162426eed791ecadef4189321b5
660710716 <lurwongsa_t@silpakorn.edu>
2025-10-11 08:48:58 +0000

    HomePage_NotLog

--- fontend/job_app/src/App.jsx
+++ fontend/job_app/src/App.jsx
@@ -8,7 +8,7 @@ import Register_Page from './pages/Register_Page';
 import Profile_Page from './pages/Profile_Page';
 import Status_Page from './pages/Status_Page';
 import HomePage from './pages/HomePage';
-
+import HomePage_NotLog from './pages/HomePage_NotLog';
 //import UserDashboard from './pages/UserDashboard';
 //import HrDashboard from './pages/HrDashboard';
 
@@ -30,6 +30,7 @@ function App() {
         <main className="flex-grow bg-gray-50">
           <Routes>
             {/* หน้า public */}
+            <Route path="/" element={<HomePage_NotLog />} />
             <Route path="/" element={<HomePage />} />
             <Route path="/" element={<Login_Page setAuth={setAuth} />} />
             <Route path="/register" element={<Register_Page />} />
